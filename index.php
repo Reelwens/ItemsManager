3b4618321db8cea756bb7cf151ad00c205b22977
Reelwens <sim.lucas27@gmail.com>
2017-03-06 01:02:54 +0100

    :lipstick: Add a favicon

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ echo '</pre>';*/
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
         <title>Gestionnaire d'items Minecraft</title>
+        <link rel="icon" type="image/png" href="src/img/favicon.png" sizes="64x64">
         <link href="https://fonts.googleapis.com/css?family=Quantico:400,400i,700,700i" rel="stylesheet">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
